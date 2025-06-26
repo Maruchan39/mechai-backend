d316eaf41f8624f7171eb9db5557401a00980788
Maruchan39 <martynas.baltutis@gmail.com>
2025-06-26 11:37:31 +0300

    Keep chat context

--- controllers/chatbot.controller.ts
+++ controllers/chatbot.controller.ts
@@ -6,39 +6,29 @@ const accessToken = requireEnv("HF_ACCESS_TOKEN");
 
 const client = new InferenceClient(accessToken);
 
-type ChatbotMessage = {
+type Message = {
   text: string;
-  author: "chatbot";
-};
-
-type UserMessage = {
-  text: string;
-  author: "user";
+  role: "user" | "chatbot";
 };
 
 export const getChatbotResponse = async (
-  req: Request<{}, {}, UserMessage>,
+  req: Request<{}, {}, Message[]>,
   res: Response
 ) => {
   try {
-    const { text } = req.body;
+    const messages = req.body;
 
     const chatCompletion = await client.chatCompletion({
       provider: "novita",
       model: "deepseek-ai/DeepSeek-V3-0324",
-      messages: [
-        {
-          role: "user",
-          content: text,
-        },
-      ],
+      messages: messages,
     });
 
-    const chatbotResponse: ChatbotMessage = {
-      text:
+    const chatbotResponse = {
+      content:
         chatCompletion.choices[0].message.content ||
         "Sorry, I have encountered while fetching the answer",
-      author: "chatbot",
+      role: "assistant",
     };
 
     console.log(chatCompletion.choices[0].message);
